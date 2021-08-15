19ad567525368eb1b845b4ca5481e05bc6dd0ae9
swuff-star <63352264+swuff-star@users.noreply.github.com>
2021-08-15 18:35:23 -0400

    thallium (broken)
    le dotindex is crash

--- LIT/Assets/LostInTransit/Modules/Buffs/BuffTypes/ThalliumPoison.cs
+++ LIT/Assets/LostInTransit/Modules/Buffs/BuffTypes/ThalliumPoison.cs
@@ -1,10 +1,7 @@
-﻿using R2API;
+﻿using LostInTransit.Modules;
+using R2API;
 using RoR2;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace LostInTransit.Buffs
 {
@@ -17,7 +14,24 @@ namespace LostInTransit.Buffs
         public override void Initialize()
         {
             buff = BuffDef;
-            index = DotAPI.RegisterDotDef(1, 0.5f, DamageColorIndex.Poison, BuffDef);
+            index = DotAPI.RegisterDotDef(1f, 0.5f, DamageColorIndex.DeathMark, BuffDef);
+        }
+
+        public override void AddBehavior(ref CharacterBody body, int stack)
+        {
+            body.AddItemBehavior<ThalDebuffBehavior>(stack);
+        }
+
+        public class ThalDebuffBehavior : CharacterBody.ItemBehavior, IStatItemBehavior
+        {
+            public void RecalcStatsEnd()
+            {
+                body.moveSpeed *= LostInTransit.Items.Thallium.slowMultiplier;
+            }
+
+            public void RecalcStatsStart()
+            {
+            }
         }
     }
 }
---
+++ LIT/Assets/LostInTransit/Modules/Pickups/Items/Thallium.cs
@@ -0,0 +1,54 @@
+﻿using LostInTransit.Modules;
+using RoR2;
+using LostInTransit.Buffs;
+
+namespace LostInTransit.Items
+{
+    public class Thallium : ItemBase
+    {
+        public override ItemDef ItemDef { get; set; } = Assets.LITAssets.LoadAsset<ItemDef>("Thallium");
+
+        public static float procChance;
+        public static float dmgCoefficient;
+        public static float dmgStack;
+        public static float slowMultiplier;
+        public static int duration;
+        public override void Initialize()
+        {
+            procChance = LITMain.config.Bind<float>("Item: " + ItemDef.name, "Proc Chance", 10f, "Chance to afflict Thallium Poisoning.").Value;
+            dmgCoefficient = LITMain.config.Bind<float>("Item: " + ItemDef.name, "Base Damage", 1.25f, "Damage coefficient of Thallium, multiplied by duration for total damage.").Value;
+            dmgStack = LITMain.config.Bind<float>("Item: " + ItemDef.name, "Stacking Damage", 0.625f, "Extra damage dealth by extra stacks.").Value;
+            slowMultiplier = LITMain.config.Bind<float>("Item: " + ItemDef.name, "Slow Amount", 0.75f, "Power of Thallium Poisoning's slow.").Value;
+            duration = LITMain.config.Bind<int>("Item: " + ItemDef.name, "Debuff Duration", 4, "Duration of the Thallium Poisoning debuff.").Value;
+        }
+        public override void AddBehavior(ref CharacterBody body, int stack)
+        {
+            body.AddItemBehavior<MysteriousVialBehavior>(stack);
+        }
+
+        public class MysteriousVialBehavior : CharacterBody.ItemBehavior, IOnDamageDealtServerReceiver
+        {
+            public void OnDamageDealtServer(DamageReport damageReport)
+            {
+                var body = damageReport.victim;
+                var dotController = DotController.FindDotController(body.gameObject);
+                bool hasDot = false;
+                if (dotController)
+                    hasDot = dotController.HasDotActive(ThalliumPoison.index);
+
+                if (Util.CheckRoll(procChance) && !hasDot)
+                {
+                    var dotInfo = new InflictDotInfo()
+                    {
+                        attackerObject = body.gameObject,
+                        victimObject = body.gameObject,
+                        dotIndex = ThalliumPoison.index,
+                        duration = duration,
+                        damageMultiplier = damageReport.victimBody.damage * (dmgCoefficient + dmgStack * (stack - 1))
+                    };
+                    DotController.InflictDot(ref dotInfo);
+                }
+            }
+        }
+    }
+}
