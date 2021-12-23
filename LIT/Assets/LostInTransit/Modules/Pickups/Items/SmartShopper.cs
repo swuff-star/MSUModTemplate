b6246686a2995737e1ef0d654bc74e4d7670c2d2
Nebby <78170922+Nebby1999@users.noreply.github.com>
2021-12-22 21:50:43 -0300

    Smart shopper but not shit
    tokens need better shit n stuff

--- LIT/Assets/LostInTransit/Modules/Pickups/Items/SmartShopper.cs
+++ LIT/Assets/LostInTransit/Modules/Pickups/Items/SmartShopper.cs
@@ -1,6 +1,7 @@
 ﻿using Moonstorm;
 using RoR2;
 using System;
+using UnityEngine;
 
 namespace LostInTransit.Items
 {
@@ -9,19 +10,69 @@ namespace LostInTransit.Items
         private const string token = "LIT_ITEM_SMARTSHOPPER_DESC";
         public override ItemDef ItemDef { get; set; } = LITAssets.Instance.MainAssetBundle.LoadAsset<ItemDef>("SmartShopper");
 
-        [ConfigurableField(ConfigName = "Money Bonus", ConfigDesc = "Amount of extra money gained, per stack.")]
+        [ConfigurableField(ConfigName = "Refund Amount", ConfigDesc = "Percentage of money refunded when purchasing something, Percentage (0.5 = 50)")]
+        [TokenModifier(token, StatTypes.Percentage, 0)]
+        public static float RefundAmount = 0.5f;
+
+        /*[ConfigurableField(ConfigName = "Money Bonus", ConfigDesc = "Amount of extra money gained, per stack.")]
         [TokenModifier(token, StatTypes.Percentage, 0)]
         public static float goldAmount = 0.25f;
 
         [ConfigurableField(ConfigName = "Use Exponential Scaling", ConfigDesc = "Whether scaling should be done exponentially or linearally.")]
-        public static bool usesExpScaling = true;
+        public static bool usesExpScaling = true;*/
 
         public override void AddBehavior(ref CharacterBody body, int stack)
         {
             body.AddItemBehavior<SmartShopperBehavior>(stack);
         }
 
-        public class SmartShopperBehavior : CharacterBody.ItemBehavior, IOnKilledOtherServerReceiver
+        public class SmartShopperBehavior : CharacterBody.ItemBehavior
+        {
+            private float refundAmount;
+            private int maxRefunds;
+            private int currentRefunds;
+
+            public bool CanRefund { get => currentRefunds < maxRefunds; }
+            private void Refund(Interactor interactor, IInteractable interactable, UnityEngine.GameObject interactableObject)
+            {
+                var pInteraction = interactableObject.GetComponent<PurchaseInteraction>();
+                if(pInteraction)
+                {
+                    if(pInteraction.costType == CostTypeIndex.Money && CanRefund)
+                    {
+                        OnRefund((uint)pInteraction.cost);
+                    }
+                }
+            }
+
+            private void OnRefund(uint moneyCost)
+            {
+                currentRefunds++;
+                body.master?.GiveMoney((uint)(moneyCost * refundAmount));
+            }
+
+            public void Start()
+            {
+                refundAmount = Mathf.Clamp01(RefundAmount);
+                currentRefunds = 0;
+                maxRefunds = stack;
+                GlobalEventManager.OnInteractionsGlobal += Refund;
+                body.onInventoryChanged += UpdateStacks;
+            }
+
+            private void UpdateStacks()
+            {
+                stack = body.inventory.GetItemCount(LITAssets.Instance.MainAssetBundle.LoadAsset<ItemDef>("SmartShopper"));
+            }
+
+            public void OnDestroy()
+            {
+                GlobalEventManager.OnInteractionsGlobal -= Refund;
+                body.onInventoryChanged -= UpdateStacks;
+            }
+        }
+
+        /*public class SmartShopperBehavior : CharacterBody.ItemBehavior, IOnKilledOtherServerReceiver
         {
             public void OnKilledOtherServer(DamageReport damageReport)
             {
@@ -37,6 +88,6 @@ namespace LostInTransit.Items
                     body.master.GiveMoney((uint)smartShopperGold);
                 }
             }
-        }
+        }*/
     }
 }
